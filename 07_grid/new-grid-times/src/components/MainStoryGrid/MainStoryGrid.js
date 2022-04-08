4d1e8b9a42994892c6608638e85507a663990d6a
TajibSmajlovic <smajlovic.tajib@gmail.com>
2022-04-08 10:21:18 +0200

    refactoring and adding CSS grid

---
+++ 07_grid/new-grid-times/src/components/MainStoryGrid/MainStoryGrid.js
@@ -0,0 +1,146 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "../../data";
+import { QUERIES } from "../../constants";
+
+import SectionTitle from "../SectionTitle";
+import MainStory from "../MainStory";
+import SecondaryStory from "../SecondaryStory";
+import OpinionStory from "../OpinionStory";
+import Advertisement from "../Advertisement";
+
+const MainStoryGrid = () => {
+  return (
+    <Wrapper>
+      <MainStorySection>
+        <MainStory {...MAIN_STORY} />
+      </MainStorySection>
+
+      <SecondaryStorySection>
+        <StoryList>
+          {SECONDARY_STORIES.map((story, index) => (
+            <BorderWrapper key={story.id}>
+              <SecondaryStory {...story} />
+            </BorderWrapper>
+          ))}
+        </StoryList>
+      </SecondaryStorySection>
+
+      <OpinionSection>
+        <SectionTitle>Opinion</SectionTitle>
+        <OpinionStoryList>
+          {OPINION_STORIES.map((story, index) => (
+            <OpinionStoryBorderWrapper key={story.id}>
+              <OpinionStory {...story} />
+            </OpinionStoryBorderWrapper>
+          ))}
+        </OpinionStoryList>
+      </OpinionSection>
+
+      <AdvertisementSection>
+        <Advertisement />
+      </AdvertisementSection>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    "main-story"
+    "secondary-stories"
+    "opinion-stories"
+    "advertisement";
+  gap: 48px;
+  margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    gap: 48px 0;
+    grid-template-columns: 2fr 1fr;
+    grid-template-areas:
+      "main-story secondary-stories"
+      "advertisement advertisement"
+      "opinion-stories opinion-stories";
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    gap: 0;
+    grid-template-columns: 5fr 4fr 3fr;
+    grid-template-areas:
+      "main-story secondary-stories opinion-stories"
+      "main-story advertisement advertisement";
+  }
+`;
+
+const MainStorySection = styled.section`
+  grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+`;
+
+const SecondaryStorySection = styled.section`
+  grid-area: secondary-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+`;
+
+const StoryList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const OpinionStoryList = styled(StoryList)`
+  @media ${QUERIES.tabletOnly} {
+    gap: 32px;
+    flex-direction: row;
+  }
+`;
+
+const OpinionSection = styled.section`
+  grid-area: opinion-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: -8px;
+  }
+`;
+
+const AdvertisementSection = styled.section`
+  grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-top: 16px;
+    margin-top: 16px;
+    border-top: 1px solid var(--color-gray-300);
+  }
+`;
+
+const BorderWrapper = styled.div`
+  &:not(:last-of-type) {
+    border-bottom: 1px solid var(--color-gray-300);
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+const OpinionStoryBorderWrapper = styled(BorderWrapper)`
+  @media ${QUERIES.tabletOnly} {
+    flex: 1;
+
+    &:not(:last-of-type) {
+      border-bottom: revert;
+      padding-bottom: revert;
+      margin-bottom: revert;
+    }
+  }
+`;
+
+export default MainStoryGrid;
