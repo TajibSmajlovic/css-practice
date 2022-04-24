c40412c9343c2dbb9bee4025eb45e2a75c730fef
TajibSmajlovic <smajlovic.tajib@gmail.com>
2022-04-24 13:54:29 +0200

    adding 08_animations section

---
+++ 08_animations/src/components/NavLink/NavLink.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+import { WEIGHTS } from "../../constants";
+
+export const NavLink = ({ children, ...delegated }) => (
+  <Wrapper {...delegated}>
+    <MainText>{children}</MainText>
+    <HoverText>{children}</HoverText>
+  </Wrapper>
+);
+
+const Wrapper = styled.a`
+  position: relative;
+  display: block;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+
+  overflow: hidden;
+
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+`;
+
+const Text = styled.span`
+  display: block;
+  transform: translateY(var(--translate-from));
+  transition: transform 350ms;
+
+  @media (hover: hover) and (prefers-reduced-motion: no-preference) {
+    ${Wrapper}:hover & {
+      transform: translateY(var(--translate-to));
+      transition: transform 200ms;
+    }
+  }
+`;
+
+const MainText = styled(Text)`
+  --translate-from: 0%;
+  --translate-to: -100%;
+`;
+
+const HoverText = styled(Text)`
+  --translate-from: 100%;
+  --translate-to: 0%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  font-weight: ${WEIGHTS.bold};
+`;
